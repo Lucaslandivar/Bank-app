a6e25d2f5e155c65d0b72a016cc424e0d9d5ae5a
Lucas Landivar <landivarlucas67@gmail.com>
2025-11-11 21:37:50 -0300

    Adding primeNG and testing a button component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import Aura from '@primeng/themes/aura';
+import lara from '@primeng/themes/lara';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    providePrimeNG({
+      theme: {
+        preset: lara,
+      },
+    }),
+  ],
 };
